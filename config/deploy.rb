6ba196d4bd9449ffa4919abac62a4901e29bbd9e
seairy <seairy@gmail.com>
2015-08-01 16:11:13 +0800

    init commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :scm, :git
 set :repo_url, 'git@github.com:seairy/Jasmine.git'
 set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/settings.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/abstract_images}
 set :use_sudo, false
 
 namespace :deploy do
